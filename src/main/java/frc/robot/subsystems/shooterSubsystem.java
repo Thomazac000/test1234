d801423d3c49e1a53f5d26bdeb04d633f1c2f753
thomazac000@usd232.org <thomazac000@usd232.org>
2024-01-20 11:14:28 -0600

    added shooter

--- src/main/java/frc/robot/subsystems/shooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooterSubsystem.java
@@ -1,6 +1,22 @@
+package frc.robot.subsystems;
+
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
 public class shooterSubsystem extends SubsystemBase{
-     
+     CANSparkMax motor1;
+     CANSparkMax motor2;
+
+     public shooterSubsystem(){
+         motor1 = new CANSparkMax(0,MotorType.kBrushless);
+         motor2 = new CANSparkMax(1,MotorType.kBrushless);
+     }
+     public void runShooter(double speed1,double speed2){
+        motor1.set(speed1);
+        motor2.set(speed2);
+     }
 }
